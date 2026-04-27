8a3eb030c2bf0f5ee363bc18a0ab63168ea00d10
Rafaljakimowicz <bobix7913@gmail.com>
2026-04-27 08:18:15 +0200

    threading

--- Kolejki/src/main.cpp
+++ Kolejki/src/main.cpp
@@ -2,13 +2,28 @@
 #include "factory/PriorityQueueTreeFactory.hpp"
 #include "factory/PriorityQueueVectorSortedFactory.hpp"
 #include <vector>
+#include <thread>
+#include "factory/IQueueFactory.hpp"
 
 int main(int argc, char ** argv){
     std::vector<int> sizes = {1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000};
     
     PriorityQueueTreeFactory<int> pqtf;
+    PriorityQueueVectorSortedFactory<int> pqvsf;
+
     EfficencyTest testTree(sizes, 100, &pqtf, "tree.csv");
-    testTree.test();
+    EfficencyTest testVector(sizes, 100, &pqvsf, "vector.csv");
+
+    std::thread treeThread(&EfficencyTest::test, &testTree);
+    std::thread vectorThread(&EfficencyTest::test, &testVector);
+
+    if(treeThread.joinable()){
+        treeThread.join();
+    }
+
+    if(vectorThread.joinable()){
+        vectorThread.join();
+    }
 
     return 0;
 }
